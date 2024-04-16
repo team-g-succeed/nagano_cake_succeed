6930ce30860a3b2c3635404795d0b389ede27819
tkykkkkk <158381693+tkykkkkk@users.noreply.github.com>
2024-04-16 14:06:05 +0000

    コントローラとルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  
   root to: 'homes#top'
   get 'homes/about'
   devise_for :users
+  
+  namespace :admin do
+   resources :items, only: [:index, :show, :new, :create, :edit, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
