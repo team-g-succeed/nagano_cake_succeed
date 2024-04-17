d9d32cec1833b0ae669a229dabcacc92e4261366
tkykkkkk <158381693+tkykkkkk@users.noreply.github.com>
2024-04-17 01:44:03 +0000

    会員側のitemコントローラ作成

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,32 @@
+class Admin::ItemsController < ApplicationController
+  
+  before_action :set_ite, only: [:show, :edit, :update]
+  
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+  end 
+  
+  def edit
+  end
+  
+  def update
+  end 
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :introduction,:price, :is_active, :image)
+  end 
+  
+end
