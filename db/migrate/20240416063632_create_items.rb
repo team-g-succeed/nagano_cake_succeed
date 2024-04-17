a610c935fe6b3ce9520723bee30b8e18e137223f
tkykkkkk <158381693+tkykkkkk@users.noreply.github.com>
2024-04-17 04:52:47 +0000

    モデル編集(admin_items→itemsに統一)

--- db/migrate/20240416063632_create_items.rb
+++ db/migrate/20240416063632_create_items.rb
@@ -2,10 +2,10 @@ class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
       t.integer :genre_id
-      t.string :item_name
-      t.integer :price
-      t.text :introduction
-      t.boolean :is_active
+      t.string :item_name, null: false
+      t.integer :price, null: false
+      t.text :introduction, null: false
+      t.boolean :is_active, null: false, default: 0
 
       t.timestamps
     end
